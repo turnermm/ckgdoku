0b5905187bb11a2cc04b547e5e51e700cb310326
Myron Turner <turnermm02@shaw.ca>
2017-04-04 11:16:43 -0500

    moves js out of mediamanager.php to scripts/mediamgr.js

--- action/mediamanager.php
+++ action/mediamanager.php
@@ -15,61 +15,30 @@ class action_plugin_ckgdoku_mediamanager extends DokuWiki_Action_Plugin {
     {
         $controller->register_hook('MEDIAMANAGER_STARTED', 'AFTER', $this, 'mediaman_started');
         $controller->register_hook('MEDIA_UPLOAD_FINISH', 'BEFORE', $this, 'upload_finish');              
+        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'handle_metafile');
     }
 
-    function upload_finish($event) {
+    function handle_metafile(Doku_Event $event) {
+          $event->data['script'][] = 
+            array( 
+                'type'=>'text/javascript', 
+                'charset'=>'utf-8', 
+                '_data'=>'',
+                 'src'=>DOKU_BASE.'lib/plugins/ckgdoku/scripts/mediamgr.js'
+            );
+    }
+    function upload_finish(Doku_Event $event) {
         if(!preg_match("#^image/#",$event->data[3]) && $_COOKIE['ckgFbType'] == 'image') {
             msg($this->getLang('mediamgr_imgonly')  .$event->data[3] );
            $event->preventDefault() ;
         }    
     }
     
-    function mediaman_started($event) {
+    function mediaman_started(Doku_Event $event) {
         if ($_GET["onselect"] == "ckg_edit_mediaman_insert") {
             setcookie('ckgFbType', 'image');
         } else if ($_GET["onselect"] == "ckg_edit_mediaman_insertlink") {
             setcookie('ckgFbType', 'link');
         }
-
-        echo '<script type="text/javascript">
-        if (opener != null && opener.CKEDITOR !== undefined) {
-            window.onload = function () {
-                jQuery( document ).ready(function() {
-                    if ((location.search.split("ckg_media=")[1]||"").split("&")[0] == "link") {
-                        jQuery(".select").on("click", function(event) {
-                            var $link, id;
-
-                            event.preventDefault();
-
-                            $link = jQuery(this);
-                            id = $link.attr("id").substr(2);
-
-                            dw_mediamanager.insert(id);
-                            return;
-                        });
-                    } else if ((location.search.split("ckg_media=")[1]||"").split("&")[0] == "img") {
-                        jQuery("#media__linkbtn4").css("display", "none");
-                        ckg_nonimage_overlay();
-                    }
-                });
-
-                jQuery(document).ajaxComplete(function() {
-                    ckg_nonimage_overlay();
-                });
-            };
-        }
-
-        function ckg_nonimage_overlay() {
-            if ((location.search.split("ckg_media=")[1]||"").split("&")[0] !== "img") {
-                return;
-            }
-
-            jQuery( ".odd, .even" ).each( function( index, element ){
-                if(!this.title.match(/\.(jpg|jpeg|png|tiff?|gif)$/)){
-                    jQuery( this ).html(LANG.plugins.ckgdoku.mediamgr_notice+": <b>" + this.title  +"</b>");
-                }
-            });
-        }
-        </script>';
     }
 }
