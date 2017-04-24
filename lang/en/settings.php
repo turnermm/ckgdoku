5e1b5b8f87c3001af9d9aed6a60b55b13f56c36f
Myron Turner <turnermm02@shaw.ca>
2017-04-23 20:28:27 -0500

    Implemented reminders of new double-click feature for Admins

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -16,7 +16,7 @@ $lang['dwedit_ns'] = "Comma separated list of namespaces and/or pages where ckgd
 $lang['acl_del'] =  "Default (box not checked) allows users with upload permission to delete media files; if box is checked, then user needs delete permission to delete from the folder.";
 $lang['auth_ci'] = "The user login id is case insensitive, that is you can login as both USER and user";
 $lang['nix_style'] = "For Windows Servers (Vista and Later).  This setting makes it possible to access data\\media through ckgdoku\\CKeditor\\userfiles, if links to media and file have been successfully created in userfiles";
-$lang['no_symlinks']  = "Disable automatic creation of symbolic links in ckgdoku/userfiles.";            
+$lang['no_symlinks']  = "Disable automatic creation of symbolic links in ckgdoku/userfiles.  This option should be disabled when updating.";            
 $lang['direction'] = 'Set Language direction in CKeditor:  <b>nocheck</b>: ckgdoku will make no changes to the default direction setting; ' 
                        . ' <b>dokuwiki</b>:  the current Dokuwiki language direction;  <b>ltr</b>: Left-to-right ; <b>rtl</b>: Right-to-left.';
 $lang['scayt_auto'] = 'Automatically enable the SCAYT spellchecker. Defaults to "on". To turn off SCAYT select "off"';
