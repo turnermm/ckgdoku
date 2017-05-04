c273df280c38de5bcfd6e3d835cc0a9730e4834a
Myron Turner <turnermm02@shaw.ca>
2017-05-04 07:39:28 -0500

    Added Geshi plugin

--- conf/default.php
+++ conf/default.php
@@ -33,7 +33,7 @@ $conf['font_options']  = 0;
 $conf['nofont_styling'] = 0;
 $conf['alt_toolbar'] = '';
 $conf['mfiles'] = 0;
-$conf['extra_plugins'] ="";
+$conf['extra_plugins'] ="Geshi";
 $conf['captcha_auth'] = 'CREATE';
 $conf['htmblock_ok'] = 0;
 
