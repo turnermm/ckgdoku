cfc6cc261c6910126199a1187c8174ddecc4b00a
Myron Turner <turnermm02@shaw.ca>
2017-10-23 09:00:43 -0500

    Fix for relative links wirh colon after dots, e.g  ..:, corrupting the the ckgedit dot-rel insertion

--- action/edit.php
+++ action/edit.php
@@ -201,7 +201,7 @@ class action_plugin_ckgdoku_edit extends DokuWiki_Action_Plugin {
             }
             }
             else $text = $draft_text;
-      $text =preg_replace("/\[\[\./ms",'[[dot-repl_',$text);   
+ //    $text =preg_replace("/\[\[\./ms",'[[dot-repl_',$text);   
      $text = preg_replace_callback(
     '/(~~NOCACHE~~|~~NOTOC~~|\{\{rss>http:\/\/.*?\}\})/ms',
      create_function(
@@ -372,6 +372,8 @@ class action_plugin_ckgdoku_edit extends DokuWiki_Action_Plugin {
        //$text = preg_replace('/%%\s*<(code|file)>\s*%%/ms', 'PERC' . "$1" . 'PERC',$text);
        $text = preg_replace('/PERCcodePERC/ms','%%&lt;code&gt;%%', $text);
        $text = preg_replace('/PERCfilePERC/ms','%%&lt;file&gt;%%', $text);
+       $text =preg_replace("/\[\[(\.+):/ms","[[$1",$text);
+       $text =preg_replace("/\[\[\./ms",'[[dot-repl_',$text);   
        $divalign = false;
        if($this->helper->has_plugin('divalign2_center')) {
            $divalign = true;
