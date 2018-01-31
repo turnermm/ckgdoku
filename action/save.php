14328043bb3f42357462547693127432f588aa29
Myron Turner <turnermm02@shaw.ca>
2018-01-31 10:21:13 -0600

    Restores query string to internal link, missing after coded for relative links

--- action/save.php
+++ action/save.php
@@ -178,7 +178,7 @@ class action_plugin_ckgdoku_save extends DokuWiki_Action_Plugin {
            '#\[\[(.*?)\]\]#ms',
                function($matches){ 
                     global $ID, $conf;      
-                
+                    $qs = "";
                     if(preg_match("/\[\[http/",$matches[0])) return $matches[0];  //not an internal link
                       if(preg_match("#\[\[.*?\|\{\{.*?\}\}\]\]#", $matches[0],$matches_1)) {  
                         if(!$this->getConf('rel_links')) { 
